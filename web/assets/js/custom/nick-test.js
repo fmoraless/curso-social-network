651d844b319dc81b1cc5ae2dcbe103e5e7c93a35
Claudia <Claudia@Claudia-PC>
2016-11-07 22:13:44 -0300

    Comprobar nick name.

--- web/assets/js/custom/nick-test.js
+++ web/assets/js/custom/nick-test.js
@@ -4,7 +4,16 @@ $(document).ready(function(){
 		var nick =this.value;
 		
 		$.ajax({
-			url: '/nick-test'
+			url: URL+'/nick-test',
+			data: {nick: nick},
+			type: 'POST',
+			success: function(response){
+				if(response == "used"){
+					$(".nick-input").css("border", "1px solid red");
+				}else{
+					$(".nick-input").css("border", "1px solid green");
+				}
+			}
 		});
 	});
 });
