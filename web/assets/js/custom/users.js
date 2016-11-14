ec8e2b7a520ac3d2e3f1b4df9d57ca8b6dbb76a0
Francisco Morales Sánchez <franticzero@gmail.com>
2016-11-14 13:37:58 -0300

    Scroll Infinito.

---
+++ web/assets/js/custom/users.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+//	alert("USERS");
+	var ias = jQuery.ias({
+		container: '.box-users',
+		item: '.user-item',
+		pagination: '.pagination',
+		next: '.pagination .next_link',
+		triggetPageThreshold: 5
+	});
+	
+	ias.extension(new IASTriggerExtension({
+		text: 'Ver más personas',
+		offset: 3
+	}));
+	
+	ias.extension(new IASSpinnerExtension({
+		src: URL+'/../assets/images/ajax-loader.gif'
+	}));
+	
+	ias.extension(new IASNoneLeftExtension({
+		text: 'No hay mas personas'
+	}));
+});
+
+
