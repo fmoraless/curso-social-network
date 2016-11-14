55c6a5d8b8a2c8f9329d475a095e8ad972f1d92d
Francisco Morales Sánchez <franticzero@gmail.com>
2016-11-14 17:16:22 -0300

    Controladores y rutas para following.

---
+++ src/AppBundle/Controller/FollowingController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use BackendBundle\Entity\Following;
+use BackendBundle\Entity\User;
+
+
+class FollowingController extends Controller {
+
+    private $session;
+
+    public function __construct() {
+        $this->session = new Session();
+    }
+	
+	public function followAction(Request $request){
+		echo "Follow action";
+		die();
+	}
+
+    
+
+}
