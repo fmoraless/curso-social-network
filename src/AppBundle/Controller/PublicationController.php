7e696a8796ca84934ab50d379f3a5ca94d5c1123
Claudia <Claudia@Claudia-PC>
2016-11-22 00:06:01 -0300

    Borrar publicaciones.

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -1,112 +1,112 @@
 <?php
+
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
-
 use AppBundle\Form\PublicationType;
 use BackendBundle\Entity\Publication;
 
-class PublicationController extends Controller{
-    
-	private $session;
+class PublicationController extends Controller {
+
+    private $session;
 
     public function __construct() {
         $this->session = new Session();
     }
-	
-    public function indexAction(Request $request){
-        $em = $this->getDoctrine()->getManager();
-		$user = $this->getUser();
-		
-        $publication = new Publication();
-        $form = $this->createForm(PublicationType::class, $publication);
-        
-		$form->handleRequest($request);
-		if($form->isSubmitted()){
-			if($form->isValid()){
-				
-				//upload image
-				$file = $form['image']->getData();
-				
-				if(!empty($file) && $file != null){
-					$ext = $file->guessExtension();
-					
-					if($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif'){
-						$file_name = $user->getId().time().".".$ext;
-						$file->move("uploads/publications/images", $file_name);
-						
-						$publication->setImage($file_name);
-					}else{
-						$publication->setImage(null);
-					}
-				}else{
-					$publication->setImage(null);
-				}
-				
-				//upload document
-				$doc = $form['document']->getData();
-				
-				if(!empty($doc) && $doc != null){
-					$ext = $doc->guessExtension();
-					
-					if($ext == 'pdf'){
-						$file_name = $user->getId().time().".".$ext;
-						$doc->move("uploads/publications/documents", $file_name);
-						
-						$publication->setDocument($file_name);
-					}else{
-						$publication->setDocument(null);
-					}
-				}else{
-					$publication->setDocument(null);
-				}
-				
-				$publication->setUser($user);
-				$publication->setCreatedAt(new \DateTime("now"));
-				
-				$em->persist($publication);
-				$flush = $em->flush();
-				
-				if($flush == null){
-					$status = 'La publicacion se ha creado correctamente !!';
-				}else{
-					$status = 'Error al añadir la publicación';
-				}
-				
-			}else{
-				$status = 'La publication no se ha creado, porque el formulario no es válido !!';
-			}
-			
-			$this->session->getFlashBag()->add("status", $status);
-			return $this->redirectToRoute('home_publications');
-		}
-        
-        $publications = $this->getPublications($request);
-		
-        return $this->render('AppBundle:Publication:home.html.twig', array(
-            'form' => $form->createView(),
-            'pagination' => $publications
-        ));
-    }
-    
-    public function getPublications($request){
+
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
-        
+
+        $publication = new Publication();
+        $form = $this->createForm(PublicationType::class, $publication);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+
+                //upload image
+                $file = $form['image']->getData();
+
+                if (!empty($file) && $file != null) {
+                    $ext = $file->guessExtension();
+
+                    if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif') {
+                        $file_name = $user->getId() . time() . "." . $ext;
+                        $file->move("uploads/publications/images", $file_name);
+
+                        $publication->setImage($file_name);
+                    } else {
+                        $publication->setImage(null);
+                    }
+                } else {
+                    $publication->setImage(null);
+                }
+
+                //upload document
+                $doc = $form['document']->getData();
+
+                if (!empty($doc) && $doc != null) {
+                    $ext = $doc->guessExtension();
+
+                    if ($ext == 'pdf') {
+                        $file_name = $user->getId() . time() . "." . $ext;
+                        $doc->move("uploads/publications/documents", $file_name);
+
+                        $publication->setDocument($file_name);
+                    } else {
+                        $publication->setDocument(null);
+                    }
+                } else {
+                    $publication->setDocument(null);
+                }
+
+                $publication->setUser($user);
+                $publication->setCreatedAt(new \DateTime("now"));
+
+                $em->persist($publication);
+                $flush = $em->flush();
+
+                if ($flush == null) {
+                    $status = 'La publicacion se ha creado correctamente !!';
+                } else {
+                    $status = 'Error al añadir la publicación';
+                }
+            } else {
+                $status = 'La publication no se ha creado, porque el formulario no es válido !!';
+            }
+
+            $this->session->getFlashBag()->add("status", $status);
+            return $this->redirectToRoute('home_publications');
+        }
+
+        $publications = $this->getPublications($request);
+
+        return $this->render('AppBundle:Publication:home.html.twig', array(
+                    'form' => $form->createView(),
+                    'pagination' => $publications
+        ));
+    }
+
+    public function getPublications($request) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
         $publications_repo = $em->getRepository('BackendBundle:Publication');
         $following_repo = $em->getRepository('BackendBundle:Following');
-        
+
 //        SELECT text FROM publications WHERE user_id = 2
 //        OR user_id IN (SELECT followed FROM following WHERE user = 2);
-        
+
         $following = $following_repo->findBy(array('user' => $user));
-        foreach ($following as $follow){
+        foreach ($following as $follow) {
             $following_array[] = $follow->getFollowed();
         }
-        
+
         $query = $publications_repo->createQueryBuilder('p')
                 ->where('p.user = (:user_id) OR p.user IN (:following)')
                 ->setParameter('user_id', $user->getId())
@@ -115,10 +115,29 @@ class PublicationController extends Controller{
                 ->getQuery();
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-                    $query,
-                    $request->query->getInt('page', 1),
-                    5
-                );
+                $query, $request->query->getInt('page', 1), 5
+        );
         return $pagination;
     }
+
+    public function removePublicationAction(Request $request, $id = null) {
+        $em = $this->getDoctrine()->getManager();
+        $publication_repo = $em->getRepository('BackendBundle:Publication');
+        $publication = $publication_repo->find($id);
+
+        if($user->getId() == $publication->getUser()->getId()) {
+            $em->remove($publication);
+            $flush = $em->flush();
+
+            if($flush == null) {
+                $status = 'La publicacion se ha borrado correctamente';
+            }else{
+                $status = 'La publicacion no se ha borrado';
+            }
+        } else {
+            $status = 'La publicacion no se ha borrado';
+        }
+        return new Response($status);
+    }
+
 }
