697c7c896357e45076b2da45b430e3f31d40ee99
Claudia <Claudia@Claudia-PC>
2016-11-26 12:34:09 -0300

    Boton "Ya no me gusta" (Dislike).

--- src/AppBundle/Controller/LikeController.php
+++ src/AppBundle/Controller/LikeController.php
@@ -37,5 +37,26 @@ class LikeController extends Controller {
         } 
         return new Response($status);
     }
+    
+    public function unlikeAction($id = null){
+        $user = $this->getUser();
+        
+        $em = $this->getDoctrine()->getManager();
+        $like_repo = $em->getRepository('BackendBundle:Like');
+        $like = $like_repo->findOneby(array(
+            'user' => $user,
+            'publication' => $id
+        ));
+        
+        $em->remove($like);
+        $flush = $em->flush();
+        
+        if($flush == null){
+            $status = 'Ya no te gusta esta publicación';
+        }else{
+            $status = 'No se ha podido desmarcar el me gusta';
+        } 
+        return new Response($status);
+    }
 
 }
