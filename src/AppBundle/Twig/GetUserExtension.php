1e00a85290f56b4ea5bd50ce7b1500204031c5c8
Claudia <Claudia@Claudia-PC>
2017-01-03 23:48:51 -0300

    Nombre de usuario de interacción

--- src/AppBundle/Twig/GetUserExtension.php
+++ src/AppBundle/Twig/GetUserExtension.php
@@ -17,7 +17,7 @@ class GetUserExtension extends \Twig_Extension {
     }
     public function getUserFilter($user_id){
         $user_repo = $this->doctrine->getRepository('BackendBundle:User');
-        $user = $like_repo->findOneBy(array(
+        $user = $user_repo->findOneBy(array(
            "id" => $user_id,
         ));
         
