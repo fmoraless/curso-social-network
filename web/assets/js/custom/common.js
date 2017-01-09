c744400d16e5d8ca1d7c11c4ce6b8dafddfeaeb5
Claudia <Claudia@Claudia-PC>
2017-01-09 00:14:46 -0300

    Mostrar las notificaciones sin leer con AJAX

---
+++ web/assets/js/custom/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+	if ($(".label-notifications").text() == 0) {
+		$(".label-notifications").addClass("hidden");
+	} else {
+		$(".label-notifications").removeClass("hidden");
+	}
+
+	
+	notifications();
+	
+	setInterval(function(){
+		notifications();
+	}, 3000);
+
+});
+
+function notifications(){
+		$.ajax({
+		url: URL + '/notifications/get',
+		type: 'GET',
+		success: function (response) {
+			$(".label-notifications").html(response);
+
+			if (response == 0) {
+				$(".label-notifications").addClass("hidden");
+			} else {
+				$(".label-notifications").removeClass("hidden");
+			}
+		}
+	});
+}
